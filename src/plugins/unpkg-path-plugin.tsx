38e45ec07cdb9457bfebc8cc382f5d968078b256
Brian O'Kelly <brianjrok@gmail.com>
2022-11-02 19:59:18 -0600

    solving relative and nested paths when bundling

--- src/plugins/unpkg-path-plugin.tsx
+++ src/plugins/unpkg-path-plugin.tsx
@@ -11,6 +11,13 @@ export const unpkgPathPlugin = () => {
           return { path: args.path, namespace: 'a' };
         }
 
+        if (args.path.includes('./') || args.path.includes('../')) {
+          return {
+            namespace: 'a',
+            path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/' ).href,
+          };
+        }
+
         return {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`,
@@ -24,16 +31,17 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('tiny-test-pkg');
+              const message = require('react');
               console.log(message);
             `,
           };
         }
 
-        const { data } = await axios.get(args.path);
+        const { data, request } = await axios.get(args.path);
         return {
           loader: 'jsx',
           contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname
         };
       });
     },
